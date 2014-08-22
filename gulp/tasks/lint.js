d9af6ac36d095e5f23c38478971759ed7b57fe57
Solomon Hawk <solomon.hawk@viget.com>
2014-08-22 18:48:08 -0400

    Exclude js templates from lint task

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -3,7 +3,7 @@ var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
-  return gulp.src(config.scripts.src, '!app/js/templates.js')
+  return gulp.src([config.scripts.src, '!app/javascripts/templates.js'])
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+    .pipe(jshint.reporter('jshint-stylish'));
 });
